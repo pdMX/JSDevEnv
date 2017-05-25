d66761b5e258d14c393f449d06e297ec03034af3
unknown <pdillane@PC060.monexfs.com>
2017-05-25 09:31:15 +0100

    added prod build scripts for webpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
 
 export default {
   debug: true,
@@ -15,10 +16,15 @@ export default {
     filename: 'bundle.js'
   },
   plugins: [
-        new HtmlWebpackPlugin({
+    // Create HTML file that includes reference to bundled JS. - can remove <script> src="bundle.js"</script> from html. webpack will add in dynamically and script tags for us
+    new HtmlWebpackPlugin({
       template: 'src/index.html',
       inject: true
-    })
+    }),
+
+    new webpack.optimize.DedupePlugin(),
+
+    new webpack.optimize.UglifyJsPlugin()
 
   ],
   module: {
